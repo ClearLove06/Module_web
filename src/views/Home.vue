f2fcdf537b6b62f2fde13690a48a8af7e0621d36
焦南方 <wx_fe032a350b0d42368e2a76fc30fe1cc4@git.code.tencent.com>
2020-05-26 10:46:39 +0800

    添加 图片裁剪

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,11 @@
         <CityPicker @selectCity="getCity" @CancelCity="canceCity"/>
       </Popup>
 
+      <Button type="primary" style="position: relative">
+        点击裁剪
+        <Cropper style="position: absolute;top:0;left: 0"  @callback="cropperData" />
+      </Button>
+      <img style="width: 100px; height: 100px" :src="imgUrl" alt="">
     </div>
   </div>
 </template>
@@ -21,6 +26,7 @@
   import { NavBar, Toast,Button ,Popup} from 'vant';
   import Video from '../components/video/Index'
   import CityPicker from '../components/picker/CityPicker.vue'
+  import Cropper from '../components/cropper/Index'
   export default {
     name: "Home",
     components:{
@@ -30,12 +36,14 @@
       Video,
       CityPicker,
       Popup,
+      Cropper,
     },
     data(){
       return {
         src:'http://vjs.zencdn.net/v/oceans.mp4',
         poster:'https://b-gold-cdn.xitu.io/v3/static/img/bg.1f516b3.png',
-        cityFlag:false
+        cityFlag:false,
+        imgUrl:'', //
       }
     },
     methods:{
@@ -53,6 +61,11 @@
       canceCity(value){
         console.log(value);
         this.cityFlag = false
+      },
+      // 裁剪回调
+      cropperData(value){
+        console.log(value);
+        this.imgUrl = value
       },
     }
   }
